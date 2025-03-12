0ac88b8f0c8129766976499ad810fc92a2b209d7
Ghi <gii.omm13@gmail.com>
2025-03-12 02:26:16 -0700

    add ip check

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -11,7 +11,17 @@ def lambda_handler(event, context):
     try:
         # get visitor ip
         visitor_ip = event['requestContext']['identity']['sourceIp']
-
+        
+        # Retrieve the current visitor count
+        response = table.get_item(Key={'id': 'visits'})
+        
+        # Get the current count or default to 0 if no record exists
+        visit_count = response.get('Item', {}).get('count', 0)
+        
+        # Ensure count is an integer (fix Decimal issue)
+        if isinstance(visit_count, Decimal):
+            visit_count = int(visit_count)
+        
         # check if ip is already used
         ip_check = ip_table.get_item(Key={'ip': visitor_ip})
 
@@ -25,16 +35,6 @@ def lambda_handler(event, context):
                 'body': json.dumps({'message':'Duplicate visit', 'visitorCount': 'visit_count'})
             }
     
-        # Retrieve the current visitor count
-        response = table.get_item(Key={'id': 'visits'})
-        
-        # Get the current count or default to 0 if no record exists
-        visit_count = response.get('Item', {}).get('count', 0)
-
-        # Ensure count is an integer (fix Decimal issue)
-        if isinstance(visit_count, Decimal):
-            visit_count = int(visit_count)
-        
         # Increment the visitor count
         visit_count += 1
         
