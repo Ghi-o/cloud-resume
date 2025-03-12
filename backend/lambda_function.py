64a8a28fb7fb126a8b496190e6687ec3a35a92dc
Ghi <gii.omm13@gmail.com>
2025-03-12 02:11:04 -0700

    add ip check

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -5,9 +5,26 @@ from decimal import Decimal
 # Initialize the DynamoDB client
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('visitor-counter')
+ip_table = dynamodb.Table('visitor-ips')
 
 def lambda_handler(event, context):
     try:
+        # get visitor ip
+        visitor_ip = event['requestContext']['identity']['sourceIp']
+
+        # check if ip is already used
+        ip_check = ip_table.get_item(Key={'ip': visitor_ip})
+
+        if 'Item' in ip_check:
+            return {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    "Content-Type": "application/json"
+                },
+                'body': json.dumps({'message':'Duplicate visit', 'visitorCount': 'unchanged'})
+            }
+    
         # Retrieve the current visitor count
         response = table.get_item(Key={'id': 'visits'})
         
@@ -23,6 +40,9 @@ def lambda_handler(event, context):
         
         # Update the count in DynamoDB
         table.put_item(Item={'id': 'visits', 'count': visit_count})
+
+        # adds visitor ip
+        ip_table.put_item(Item={'ip': visitor_ip})
         
         # Return the updated count
         return {
@@ -39,5 +59,3 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps({'error': str(e)})
         }
-    
-print("Lambda updated by GitHub Actions!")
